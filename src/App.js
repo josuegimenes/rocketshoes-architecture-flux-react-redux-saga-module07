cecf124a051299e6f2250cdabae6ddd488f8bbd2
Josué Gimenes <josuegalvares@gmail.com>
2019-10-24 12:14:38 -0300

    Finalizing Integration Reactotron with Redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+
+/* Redux */
 import { Provider } from 'react-redux';
+
+/* Reactotron */
+import './config/ReactotronConfig';
 
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,11 @@ import { createStore } from 'redux';
 
 import rootReducer from './modules/rootReducer';
 
-const store = createStore(rootReducer);
+/* Configurando Reactotron-Redux para rodar somente em ambiente dev */
+const enhancer =
+  process.env.NODE_ENV === 'development' ? console.tron.createEnhancer() : null;
+/* FIM */
+
+const store = createStore(rootReducer, enhancer);
 
 export default store;
