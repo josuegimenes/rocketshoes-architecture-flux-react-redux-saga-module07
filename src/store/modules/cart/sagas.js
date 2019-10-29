ad908b19f8415d98a71d460247ad4082d06b85da
Josué Gimenes <josuegalvares@gmail.com>
2019-10-29 10:31:18 -0300

    Check stock on update

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify';
 import api from '../../../services/api';
 import { formatPrice } from '../../../util/format';
 
-import { addToCartSuccess, updateAmount } from './actions';
+import { addToCartSuccess, updateAmountSuccess } from './actions';
 
 /**
  * O asterisco ao final da function é como um 'async function', porém,
@@ -37,7 +37,7 @@ function* addToCart({ id }) {
   }
 
   if (productExists) {
-    yield put(updateAmount(id, amount));
+    yield put(updateAmountSuccess(id, amount));
   } else {
     /**
      * O redux-saga não aceita o método 'api.get()' para uma chamada assíncrona,
@@ -55,4 +55,29 @@ function* addToCart({ id }) {
   }
 }
 
-export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
+function* updateAmount({ id, amount }) {
+  const productExists = yield select(state =>
+    state.cart.find(p => p.id === id)
+  );
+
+  if (amount <= 0) return;
+
+  const stock = yield call(api.get, `stock/${id}`);
+  const stockAmount = stock.data.amount;
+
+  if (amount > stockAmount) {
+    const { title } = productExists;
+
+    toast.error(
+      `O produto ${title} possui apenas ${stockAmount} itens no estoque!`
+    );
+    return;
+  }
+
+  yield put(updateAmountSuccess(id, amount));
+}
+
+export default all([
+  takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
+]);
